3e32f8df803ad385f6600065b4cdea4a7619bba2
William <wmpires@proton.me>
2023-11-19 08:22:27 +0000

    sass + bootstrap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const path = require('path')
+ 
+module.exports = {
+  output: 'export',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
